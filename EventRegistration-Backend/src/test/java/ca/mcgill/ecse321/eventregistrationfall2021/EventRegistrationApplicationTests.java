b430dc2d18b5c44c373fc3d88d5622f0094520fe
anthony-ubah <anthon.ubah@mail.mcgill.ca>
2021-09-28 16:19:28 -0400

    Fixes

--- EventRegistration-Backend/src/test/java/ca/mcgill/ecse321/eventregistrationfall2021/EventRegistrationApplicationTests.java
+++ EventRegistration-Backend/src/test/java/ca/mcgill/ecse321/eventregistrationfall2021/EventRegistrationApplicationTests.java
@@ -3,10 +3,10 @@ package ca.mcgill.ecse321.eventregistrationfall2021;
 //import org.junit.jupiter.api.Test;
 //import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 class EventRegistrationApplicationTests {
 
-	//@Test
+	@Test
 	void contextLoads() {
 	}
 
